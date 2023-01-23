df53d8c9e10e5a91343251fd4cada9c11ab0da1f
Joao Willamy Evangelista De Sousa <willamy.sousa@ifood.com.br>
2023-01-22 23:23:42 -0300

    feat: add frontend

---
+++ apps/frontend/pages/index.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  .page {
+  }
+`;
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.styled-components file.
+   */
+  return (
+    <StyledPage>
+      oi
+    </StyledPage>
+  );
+}
+
+export default Index;
