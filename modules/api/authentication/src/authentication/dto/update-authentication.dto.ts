2dfc430994d232572fd172f9618573263c5b17d5
Joao Willamy Evangelista De Sousa <willamy.sousa@ifood.com.br>
2023-01-23 14:59:40 -0300

    feat: create user

--- modules/api/authentication/src/authentication/dto/update-authentication.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAuthenticationDto } from './create-authentication.dto';
-
-export class UpdateAuthenticationDto extends PartialType(
-  CreateAuthenticationDto
-) {}
