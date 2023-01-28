20556202185fc60b61ea065035354a508bad0b29
Joao Willamy Evangelista De Sousa <willamy.sousa@ifood.com.br>
2023-01-28 17:40:28 -0300

    fix: refactor configs

--- modules/api/infrastructure/src/index.ts
+++ modules/api/infrastructure/src/index.ts
@@ -1,7 +1,8 @@
-export { LoggerInterceptor } from './logger.interceptor';
+export { LoggerInterceptor } from './logger/logger.interceptor';
 export * from './api-infrastructure.module';
-export * from './typeorm-ex.module'
-export * from './typeorm-ex.decorator'
-export { BaseQueryParametersDto } from './base-query-parameters.dto';
-export { baseQueryBuilder } from './base-query-builder';
-export { winstonConfig } from './winston.config';
+export * from './typeorm/typeorm-ex.module'
+export * from './typeorm/typeorm-ex.decorator'
+export { BaseQueryParametersDto } from './query/base-query-parameters.dto';
+export { baseQueryBuilder } from './query/base-query-builder';
+export { winstonConfig } from './logger/winston.config';
+export { mailerConfig } from './mailer/mailer.config'
